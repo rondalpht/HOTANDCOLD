9663e443ae8c30eb9ac66eeaeb18ab1a002709a4
Rondalph <rondalpht@nycstudents.net>
2023-10-04 10:36:48 -0400

    Intial Commit: Adding All Files

---
+++ ch.js
@@ -0,0 +1,7 @@
+const input=document.querySelector("button");
+input.addEventListener("click",hotorcold);
+function hotorcold(){
+    let y= Math.floor(Math.random()*100+1);
+    let x=prompt("What is your number");
+ 
+}
