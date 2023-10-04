165e2669dcea12cd26616bd0cc41ab381ae60816
Rondalph <rondalpht@nycstudents.net>
2023-10-04 10:50:24 -0400

    commited#1

--- ch.js
+++ ch.js
@@ -1,7 +1,16 @@
 const input=document.querySelector("button");
 input.addEventListener("click",hotorcold);
+const horc=document.querySelector("answer");
 function hotorcold(){
     let y= Math.floor(Math.random()*100+1);
     let x=prompt("What is your number");
- 
+    if(y==x){
+        horc.textContent="DAMN YOU'RE HOT AS HELL YOU WIN"
+    }else if(Math.abs(y-x)<10){
+    horc.textContent="You're super hot"
+    } else if(Math.abs(y-x)<25){
+        horc.textContent="You're warming up"
+    } else if(Math.abs(y-x)<25){
+        horc.textContent="You're warming up"
+    }
 }
